2ac87708575c189756223a32838400ed75bb9de5
Owen Turkle <owen@turkle.net>
2017-09-13 13:23:00 -0700

    Update calculator.rb

--- 101 Programming Foundations/Lesson 2: Small Programs/calculator.rb
+++ 101 Programming Foundations/Lesson 2: Small Programs/calculator.rb
@@ -17,14 +17,14 @@ number2 = Kernel.gets().chomp()
 Kernel.puts("What operation do you want to perform: 1) add 2) subtract 3) multiply 4) divide")
 operation = Kernel.gets().chomp()
 
-if operation == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operation == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operation == '3'
-  result = number1.to_i() * number2.to_i()
-else
-  result = number1.to_f() / number2.to_f()
-end
+result =  if operation == '1'
+            number1.to_i() + number2.to_i()
+          elsif operation == '2'
+            number1.to_i() - number2.to_i()
+          elsif operation == '3'
+            number1.to_i() * number2.to_i()
+          else
+            number1.to_f() / number2.to_f()
+          end
 
 Kernel.puts("The result is #{result}")

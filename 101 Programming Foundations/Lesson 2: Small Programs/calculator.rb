3ea6aeff96057f5224427b4019e0406e75c75f16
Owen Turkle <owen@turkle.net>
2017-09-15 12:50:09 -0700

    Add bonus features to calculator.rb

--- 101 Programming Foundations/Lesson 2: Small Programs/calculator.rb
+++ 101 Programming Foundations/Lesson 2: Small Programs/calculator.rb
@@ -1,6 +1,10 @@
 require 'yaml'
 MESSAGES = YAML.load_file('calc_messages.yml')
 
+def messages(message, language='spanish')
+  MESSAGES[language][message]
+end
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -19,57 +23,57 @@ end
 
 def operation_to_message(op)
   message = case op
-            when '1' then 'Adding'
-            when '2' then 'Subtracting'
-            when '3' then 'Multiplying'
-            when '4' then 'Dividing'
+            when '1' then 'adding'
+            when '2' then 'subtracting'
+            when '3' then 'multiplying'
+            when '4' then 'dividing'
             end
   message
 end
 
-prompt(MESSAGES['welcome'])
+prompt(messages('welcome'))
 
 name = ''
 loop do
   name = Kernel.gets().chomp()
 
   if name.empty?()
-    prompt(MESSAGES['valid_name'])
+    prompt(messages('valid_name'))
   else
     break
   end
 end
 
-prompt(MESSAGES['hello_name'] + "#{name}!")
+prompt(messages('hello_name') + "#{name}!")
 
 loop do # outer loop
   number1 = ''
 
   loop do
-    prompt(MESSAGES['first_number_prompt'])
+    prompt(messages('first_number_prompt'))
     number1 = Kernel.gets().chomp()
 
     if number?(number1)
       break
     else
-      prompt(MESSAGES['valid_number'])
+      prompt(messages('valid_number'))
     end
   end
 
   number2 = ''
 
   loop do
-    prompt(MESSAGES['second_number_prompt'])
+    prompt(messages('second_number_prompt'))
     number2 = Kernel.gets().chomp()
 
     if number?(number2)
       break
     else
-      prompt(MESSAGES['valid_number'])
+      prompt(messages('valid_number'))
     end
   end
 
-  prompt(MESSAGES['operation_prompt'])
+  prompt(messages('operation_prompt'))
 
   operation = ''
   loop do
@@ -78,11 +82,11 @@ loop do # outer loop
     if %w(1 2 3 4).include?(operation)
       break
     else
-      prompt(MESSAGES['operation_choice'])
+      prompt(messages('operation_choice'))
     end
   end
 
-  prompt("#{operation_to_message(operation)}" + MESSAGES['operation_to_message'])
+  prompt(messages(operation_to_message(operation)))
 
   result = case operation
            when '1' then number1.to_f() + number2.to_f()
@@ -91,11 +95,11 @@ loop do # outer loop
            when '4' then number1.to_f() / number2.to_f()
            end
 
-  prompt(MESSAGES['result'] + "#{result}")
+  prompt(messages('result') + "#{result}")
 
-  prompt(MESSAGES['calculate_loop'])
+  prompt(messages('calculate_loop'))
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
 
-prompt(MESSAGES['exit'])
+prompt(messages('exit'))

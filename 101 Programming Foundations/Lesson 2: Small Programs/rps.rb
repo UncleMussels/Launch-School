48ee9c7fa998b24f6e05e3cae8eb9b49016ba3e4
Owen Turkle <owen@turkle.net>
2017-09-20 11:57:36 -0700

    Update rps w/ bonus features

--- 101 Programming Foundations/Lesson 2: Small Programs/rps.rb
+++ 101 Programming Foundations/Lesson 2: Small Programs/rps.rb
@@ -1,39 +1,65 @@
-# Simple rock, paper, scissors game vs. cpu
+# Simple rock, paper, scissors, lizard, spock game vs. cpu
 
-WEAPON_CHOICES = %w(rock paper scissors)
+WEAPON_CHOICES = %w(rock paper scissors spock lizard)
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def display_results(player, opponent)
-  if  (player == 'rock' && opponent == 'scissors') ||
-      (player == 'paper' && opponent == 'rock') ||
-      (player == 'scissors' && opponent == 'paper')
-    prompt("You win! Press 'y' to play again")
-  elsif player == opponent
-    prompt("It's a draw! Press 'y' to play again")
+def print_winner(player, cpu)
+  if player == 5
+    prompt("You won, #{player} to #{cpu}! Press 'y' to play again")
   else
-    prompt("Opponent wins! Press 'y' to play again")
+    prompt("You lost, #{cpu} to #{player}! Press 'y' to play again")
   end
 end
 
-loop do
-  prompt("Choose your weapon: #{WEAPON_CHOICES.join(', ')}")
+winning_moves = {
+  rock: %w(lizard scissors),
+  paper: %w(rock spock),
+  scissors: %w(paper lizard),
+  lizard: %w(spock paper),
+  spock: %w(rock scissors)
+}
 
-  choice = ''
-  loop do
-    choice = gets.chomp.downcase
-    break if WEAPON_CHOICES.include?(choice)
-    prompt("You must choose a weapon to play: #{WEAPON_CHOICES.join(' or ')}")
+player_wins = 0
+cpu_wins = 0
+
+prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock. " \
+       "First to 5 wins!")
+
+loop do
+  until player_wins == 5 || cpu_wins == 5
+    choice = ''
+
+    prompt("Choose your weapon: #{WEAPON_CHOICES.join(', ')}")
+    loop do
+      choice = gets.chomp.downcase
+      break if WEAPON_CHOICES.include?(choice)
+      prompt("You must choose a weapon to play: #{WEAPON_CHOICES.join(' or ')}")
+    end
+
+    cpu_choice = WEAPON_CHOICES.sample
+
+    result = if winning_moves[choice.to_s].include?(cpu_choice.to_s)
+               "You win!"
+             elsif choice == cpu_choice
+               "It's a draw!"
+             else
+               "Opponent wins!"
+             end
+
+    puts "You chose #{choice.upcase}; " \
+         "opponent chose #{cpu_choice.upcase}... #{result}"
+
+    if result.start_with?('You')
+      player_wins += 1
+    elsif result.start_with?('Opponent')
+      cpu_wins += 1
+    end
   end
 
-  computer_choice = WEAPON_CHOICES.sample
-
-  prompt("You chose #{choice.upcase}; opponent chose #{computer_choice.upcase}")
-
-  display_results(choice, computer_choice)
-
+  print_winner(player_wins, cpu_wins)
   break unless gets.chomp.downcase.start_with?('y')
 end
 

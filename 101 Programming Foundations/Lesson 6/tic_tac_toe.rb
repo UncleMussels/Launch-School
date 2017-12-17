3af7722ba7e5988a0ad59d0f4ceec3fdd35532c6
Owen Turkle <owen@turkle.net>
2017-12-17 13:29:00 -0800

    Refactor Tic Tac Toe

--- 101 Programming Foundations/Lesson 6/tic_tac_toe.rb
+++ 101 Programming Foundations/Lesson 6/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 # Tic Tac Toe program - Lesson 6
 
-USER_CHOICE = 'x'
-CPU_CHOICE = 'o'
+USER = 'x'
+CPU = 'o'
 WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
                   [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -26,97 +26,104 @@ def show_board(board_spaces)
   puts ends
 end
 
-def valid_input?(input)
-  (input.to_i.to_s == input) && (1..9).cover?(input.to_i)
+def clear_board
+  system('clear') || system('cls')
 end
 
-def valid_square?(square, board_spaces)
-  (board_spaces[square].to_i.to_s == board_spaces[square])
+def valid_input?(input)
+  (input.to_i.to_s == input) && (1..9).include?(input.to_i)
+end
+
+def empty_square?(square, board_spaces)
+  board_spaces[square] == ' '
 end
 
 def user_turn(board_spaces)
   loop do
-    square = gets.chomp
+    input = gets.chomp
 
-    if valid_input? square
-      square = square.to_i - 1
-      if valid_square? square, board_spaces
-        board_spaces[square] = USER_CHOICE
+    if valid_input? input
+      square = input.to_i - 1
+      if empty_square? square, board_spaces
+        board_spaces[square] = USER
         break
       else
-        prompt "That square is taken! Please select a numbered square: "
+        prompt "That square is taken! Please select an empty square: "
       end
     else
-      prompt "Invalid input! Please select a numbered square: "
+      prompt "Invalid input! Please select a square from 1 to 9: "
     end
   end
 end
 
 def cpu_turn(board_spaces)
-  square = board_spaces.select { |s| s.to_i.to_s == s }.sample.to_i
-  board_spaces[square - 1] = CPU_CHOICE
-  square
+  loop do
+    square = rand(10)
+    if empty_square?(square, board_spaces)
+      board_spaces[square] = CPU
+      return square
+    end
+  end
 end
 
-def check_board(board_spaces)
+def game_result(board_spaces)
   return "You win!" if user_win? board_spaces
   return "Computer wins!" if cpu_win? board_spaces
   return "It's a tie!" if tie? board_spaces
 end
 
-def user_win?(board_spaces)
+def game_over?(player, board_spaces)
   WINNING_COMBOS.any? do |combo|
     combo.all? do |space|
-      board_spaces[space] == USER_CHOICE
+      board_spaces[space] == player
     end
   end
+end
+
+def user_win?(board_spaces)
+  game_over? USER, board_spaces
 end
 
 def cpu_win?(board_spaces)
-  WINNING_COMBOS.any? do |combo|
-    combo.all? do |space|
-      board_spaces[space] == CPU_CHOICE
-    end
-  end
+  game_over? CPU, board_spaces
 end
 
 def tie?(board_spaces)
-  board_spaces.none? { |space| space.to_i.to_s == space }
+  board_spaces.none? { |square| square == ' ' }
 end
 
 loop do
   puts "Welcome to Tic Tac Toe! You will be 'x' and computer will be 'o'!"
 
   loop do
-    board_spaces = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    board_spaces = %w(1 2 3 4 5 6 7 8 9)
     show_board board_spaces
+    board_spaces = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
     loop do
-      prompt "Choose a numbered square to mark: "
+      prompt "Choose a square to mark: "
       user_turn board_spaces
 
-      system('clear') || system('cls')
+      clear_board
 
-      break if !check_board(board_spaces).nil?
+      break if game_result(board_spaces)
 
       cpu_choice = cpu_turn board_spaces
       show_board board_spaces
-      puts "Computer chooses: #{cpu_choice}"
+      puts "Computer chooses: #{cpu_choice + 1}"
 
-      break if !check_board(board_spaces).nil?
+      break if game_result(board_spaces)
     end
 
-    if check_board(board_spaces) != "Computer wins!"
-      show_board(board_spaces)
-    end
+    show_board(board_spaces) if game_result(board_spaces) != "Computer wins!"
 
-    puts check_board board_spaces
+    puts game_result board_spaces
     break
   end
 
   prompt "Press 'y' to play again: "
   break unless gets.chomp.downcase == 'y'
-  system('clear') || system('cls')
+  clear_board
 end
 
 puts "Goodbye and thanks for playing!"
